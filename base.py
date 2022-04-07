a46bfefd8b2d3d547054d5f2feb3152cca69e7a4
Nick Shaheed <nicholasshaheed@gmail.com>
2022-04-06 21:35:52 -0700

    got osc server working

--- base.py
+++ base.py
@@ -3,14 +3,30 @@ import matplotlib.pyplot as plt
 import torchvision
 import time
 
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
 use_gpu = True if torch.cuda.is_available() else False
+
+def print_volume_handler(unused_addr, args, volume):
+      print("[{0}] ~ {1}".format(args[0], volume))
+
+ip = "127.0.0.1"
+port = 5005
+
+dispatcher = dispatcher.Dispatcher()
+dispatcher.map("/volume", print_volume_handler, "Volume")
+
+server = osc_server.ThreadingOSCUDPServer(
+    (ip, port), dispatcher)
+print("Serving on {}".format(server.server_address))
+server.serve_forever()
 
 # trained on high-quality celebrity faces "celebA" dataset
 # this model outputs 512 x 512 pixel images
 model = torch.hub.load('facebookresearch/pytorch_GAN_zoo:hub',
                        'PGAN', model_name='celebAHQ-512',
                        pretrained=True, useGPU=use_gpu)
-
 
 figure, ax = plt.subplots()
 
@@ -20,7 +36,7 @@ num_images = 1
 
 while True:
     noise, _ = model.buildNoiseData(num_images)
-    
+
     with torch.no_grad():
         generated_images = model.test(noise)
     
